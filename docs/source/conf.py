4eec8df3f6a733b1ab63ecefefa53b2ff7943218
Moira <giantsilkmoth@proton.me>
2023-10-09 20:52:02 +0200

    fixed sphinx config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = "0.1"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_automodapi.automodapi"]
+extensions = []
 numpydoc_show_class_members = False
 
 templates_path = ["_templates"]
